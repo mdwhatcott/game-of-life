93fcdfaaca508db06e5de3cdd360c7e7f70988e9
Michael Whatcott <mdwhatcott@gmail.com>
2021-08-30 16:21:09 -0600

    Project updates.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject life "0.1.0-SNAPSHOT"
+(defproject game-of-life "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
