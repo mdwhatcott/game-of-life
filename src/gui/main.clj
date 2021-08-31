1ee51703caf76fd5e1b3adb984289a21adde971f
Michael Whatcott <mdwhatcott@gmail.com>
2021-08-31 15:43:26 -0600

    Extracted constants.

--- src/gui/main.clj
+++ src/gui/main.clj
@@ -2,22 +2,52 @@
   (:require
     [quil.core :as q]
     [quil.middleware :as m]
-    [gui.frame-count :as frames]
-    [gui.controller :as controller]
     [gui.bounds :as bounds]
+    [gui.controller :as controller]
+    [gui.frame-count :as frame-count]
+    [gui.game :as game]
     [gui.grid :as grid]
     [gui.mouse :as mouse]))
 
+(def window-width 500)
+(def window-height 600)
+(def half-width (int (/ window-width 2)))
+(def grid-center [half-width half-width])
+(def control-panel-center [half-width 550])
+(def control-panel-height (- window-height window-width))
+
+; 100px-tall strip at bottom of window
+(def control-panel-bounds
+  (bounds/bounding-box control-panel-center
+                       window-width
+                       control-panel-height))
+
+; 500px square above control panel
+(def grid-bounds
+  (bounds/bounding-cube grid-center window-width))
+
+(def squares-per-row 50)
+
+(def frames-per-evolution 10)
+
 (defn setup-root []
   (-> (mouse/setup {})
-      (frames/setup 10)
-      (controller/setup (bounds/bounding-box [250 550] 500 100))
-      (grid/setup (bounds/bounding-cube [250 250] 500) 50)))
+      (frame-count/setup frames-per-evolution)
+      (controller/setup control-panel-bounds)
+      (grid/setup grid-bounds squares-per-row)))
 
 (defn update-root [state]
-  )
+  (-> state
+      (mouse/update_ (q/mouse-pressed?) (q/mouse-x) (q/mouse-y))
+      (frame-count/update_)
+      (controller/update_)
+      (grid/update_)
+      (game/update_)))
 
 (defn draw-root [state]
+  ; draw grid
+  ; draw live cells
+  ; when :stopped: draw 'click down here text'
   )
 
 (declare game-of-life)
@@ -26,7 +56,7 @@
   (q/defsketch
     game-of-life
     :title "John Conway's Game of Life"
-    :size [500 600]
+    :size [window-width window-height]
     :setup #'setup-root
     :update #'update-root
     :draw #'draw-root
