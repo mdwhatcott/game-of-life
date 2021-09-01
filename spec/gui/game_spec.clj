4bff7b0cc5977a6db58d6a61990b2459500a2411
Michael Whatcott <mdwhatcott@gmail.com>
2021-09-01 09:44:19 -0600

    Remove cells that spring up off-grid.

--- spec/gui/game_spec.clj
+++ spec/gui/game_spec.clj
@@ -30,6 +30,7 @@
                   :frame-count          0
                   :player               :playing
                   :game                 #{[1 1] [2 1] [3 1]}}
+          input  (gui-grid/setup input (bounds/bounding-cube [50 50] 100) 10)
           states (take 4 (iterate update-all input))
           games  (map :game states)
           grids  (map :grid states)
@@ -44,5 +45,53 @@
       (should= (nth grids 2) (nth grids 3))
       (should-not= (nth grids 0) (nth grids 2))
       (should-not= (nth grids 1) (nth grids 3))))
+
+  (it "filters cells with y values that are too low for the grid (negative)"
+    (let [input  {:frames-per-evolution 1
+                  :frame-count          0
+                  :player               :playing
+                  :game                 #{[1 0] [2 0] [3 0]}} ; top-most row
+          input  (gui-grid/setup input (bounds/bounding-cube [50 50] 100) 10)
+          result (update-all input)]
+      (should= #{[2 0]
+                 [2 1]} (:game result))
+      (should= [[[20 0] [30 10]]
+                [[20 10] [30 20]]] (:live-cells (:grid result)))))
+
+  (it "filters cells with x values that are too low for the grid (negative)"
+    (let [input  {:frames-per-evolution 1
+                  :frame-count          0
+                  :player               :playing
+                  :game                 #{[0 0] [0 1] [0 2]}} ; left-most column
+          input  (gui-grid/setup input (bounds/bounding-cube [50 50] 100) 10)
+          result (update-all input)]
+      (should= #{[0 1]
+                 [1 1]} (:game result))
+      (should= [[[10 10] [20 20]]
+                [[0 10] [10 20]]] (:live-cells (:grid result)))))
+
+  (it "filters cells with x values that are too high for the grid"
+    (let [input  {:frames-per-evolution 1
+                  :frame-count          0
+                  :player               :playing
+                  :game                 #{[9 0] [9 1] [9 2]}} ; right-most column
+          input  (gui-grid/setup input (bounds/bounding-cube [50 50] 100) 10)
+          result (update-all input)]
+      (should= #{[8 1]
+                 [9 1]} (:game result))
+      (should= [[[80 10] [90 20]]
+                [[90 10] [100 20]]] (:live-cells (:grid result)))))
+
+  (it "filters cells with y values that are too high for the grid"
+    (let [input  {:frames-per-evolution 1
+                  :frame-count          0
+                  :player               :playing
+                  :game                 #{[0 9] [1 9] [2 9]}} ; bottom-most row
+          input  (gui-grid/setup input (bounds/bounding-cube [50 50] 100) 10)
+          result (update-all input)]
+      (should= #{[1 8]
+                 [1 9]} (:game result))
+      (should= [[[10 90] [20 100]]
+                [[10 80] [20 90]]] (:live-cells (:grid result)))))
 
   )
--- src/gui/game.clj
+++ src/gui/game.clj
@@ -13,13 +13,23 @@
         updated    (assoc state :game (set game-cells))]
     updated))
 
+(defn is-outlier? [cell row-count]
+  (or (< (first cell) 0)
+      (< (second cell) 0)
+      (>= (first cell) row-count)
+      (>= (second cell) row-count)))
+
+(defn remove-outlier-cells [row-count cells]
+  (set (remove #(is-outlier? % row-count) cells)))
+
 (defn advance-game [state]
-  (let [game1 (:game state)
-        game2 (rules/evolve game1)
-        grid1 (:grid state)
-        cells (map #(gui-grid/game-cell->grid-cell % grid1) game2)
-        ; TODO: filter out cells outside the grid
-        grid2 (assoc grid1 :live-cells cells)]
+  (let [row-count (get-in state [:grid :cell-row-count])
+        game1     (:game state)
+        game2     (rules/evolve game1)
+        game2     (remove-outlier-cells row-count game2)
+        grid1     (:grid state)
+        cells     (map #(gui-grid/game-cell->grid-cell % grid1) game2)
+        grid2     (assoc grid1 :live-cells cells)]
     (assoc state :game game2
                  :grid grid2)))
 
