79a4a1b1043d6c47fa08e28f314c3a5869262628
Michael Whatcott <mdwhatcott@gmail.com>
2021-09-02 10:28:22 -0600

    Wrapped quil drawing behind functions. Verifying every rendered shape.

--- spec/gui/draw_spec.clj
+++ spec/gui/draw_spec.clj
@@ -4,7 +4,65 @@
     [speclj.core :refer :all]
     [gui.draw :refer :all]))
 
-(def test-drawing-state
+(describe "Drawing Shapes"
+  (with-stubs)
+
+  (it "draws a rectangle"
+    (let [rectangle {:shape  :rectangle
+                     :fill   1
+                     :stroke 2
+                     :x      3
+                     :y      4
+                     :width  5
+                     :height 6}
+          fill      (stub :fill)
+          stroke    (stub :stroke)
+          rect      (stub :rect)]
+      (with-redefs [q/fill   fill
+                    q/stroke stroke
+                    q/rect   rect]
+
+        (render-shapes [rectangle])
+
+        (should-have-invoked :fill {:with [1] :times 1})
+        (should-have-invoked :stroke {:with [2] :times 1})
+        (should-have-invoked :rect {:with [3 4 5 6] :times 1}))))
+
+  (it "draws text"
+    (let [shape      {:shape   :text
+                      :fill    1
+                      :x-align :center
+                      :y-align :center
+                      :text    "message"
+                      :x       2
+                      :y       3}
+          fill       (stub :fill)
+          text-align (stub :text-align)
+          text       (stub :text)]
+      (with-redefs [q/fill       fill
+                    q/text-align text-align
+                    q/text       text]
+
+        (render-shapes [shape])
+
+        (should-have-invoked :fill {:with [1] :times 1})
+        (should-have-invoked :text-align {:with [:center :center] :times 1})
+        (should-have-invoked :text {:with ["message" 2 3] :times 1}))))
+
+  (it "draws a background"
+    (let [shape      {:shape :background
+                      :color 42}
+          background (stub :background)]
+      (with-redefs [q/background background]
+
+        (render-shapes [shape])
+
+        (should-have-invoked :background {:with [42] :times 1}))))
+
+  )
+
+
+(def input-drawing-state
   {
    :grid {:cell-width 10
           :intro-grid [[0 0]
@@ -16,71 +74,55 @@
                        [[6 6] nil]
                        [[7 7] nil]]}})
 
-(defn assert-background-drawn []
-  (should-have-invoked :background {:with [240] :times 1}))
+(def expected-shapes-before-game-start
+  [
+   ; background
+   {:shape :background, :color 240}
 
-(defn assert-intro-grid-drawn [times]
-  (should-have-invoked :stroke {:with [128] :times times})
-  (should-have-invoked :fill {:with [240] :times times})
-  (should-have-invoked :rect {:with [0 0 10 10] :times times})
-  (should-have-invoked :rect {:with [1 1 10 10] :times times})
-  (should-have-invoked :rect {:with [2 2 10 10] :times times})
-  (should-have-invoked :rect {:with [3 3 10 10] :times times}))
+   ; grid lines
+   {:shape :rectangle, :fill 240, :stroke 128, :x 0, :y 0, :width 10, :height 10}
+   {:shape :rectangle, :fill 240, :stroke 128, :x 1, :y 1, :width 10, :height 10}
+   {:shape :rectangle, :fill 240, :stroke 128, :x 2, :y 2, :width 10, :height 10}
+   {:shape :rectangle, :fill 240, :stroke 128, :x 3, :y 3, :width 10, :height 10}
 
-(defn assert-intro-text-drawn [times]
-  (should-have-invoked :fill {:with [0] :times times})
-  (should-have-invoked :text-align {:with [:center :center] :times times})
-  (should-have-invoked :text {:with [click-here-text 250 550] :times times}))
+   ; intro text
+   {:shape :text, :fill 0, :x-align :center, :y-align :center, :x 250, :y 550,
+    :text  "Click a few squares, then click down here to begin!"}
 
-(defn assert-intro-erased-out [times]
-  (should-have-invoked :rect {:with [0 500 500 100] :times times}))
+   ; live cells
+   {:shape :rectangle, :fill 50, :stroke 50, :x 4, :y 4, :width 10, :height 10}
+   {:shape :rectangle, :fill 50, :stroke 50, :x 5, :y 5, :width 10, :height 10}
+   {:shape :rectangle, :fill 50, :stroke 50, :x 6, :y 6, :width 10, :height 10}
+   {:shape :rectangle, :fill 50, :stroke 50, :x 7, :y 7, :width 10, :height 10}])
 
-(defn assert-live-cells-drawn []
-  (should-have-invoked :fill {:with [50]})
-  (should-have-invoked :stroke {:with [50]})
-  (should-have-invoked :rect {:with [4 4 10 10] :times 1})
-  (should-have-invoked :rect {:with [5 5 10 10] :times 1})
-  (should-have-invoked :rect {:with [6 6 10 10] :times 1})
-  (should-have-invoked :rect {:with [7 7 10 10] :times 1}))
+(def expected-shapes-after-game-start
+  [
+   ; background
+   {:shape :background, :color 240}
 
-(describe "Drawing"
+   ; blacked-out intro text
+   {:shape :rectangle, :fill 50, :stroke 50, :x 0 :y 500, :width 500, :height 100}
+
+   ; live cells
+   {:shape :rectangle, :fill 50, :stroke 50, :x 4, :y 4, :width 10, :height 10}
+   {:shape :rectangle, :fill 50, :stroke 50, :x 5, :y 5, :width 10, :height 10}
+   {:shape :rectangle, :fill 50, :stroke 50, :x 6, :y 6, :width 10, :height 10}
+   {:shape :rectangle, :fill 50, :stroke 50, :x 7, :y 7, :width 10, :height 10}])
+
+(describe "Preparing Shapes"
   (with-stubs)
 
-  (context "Introductory Elements"
-    (it "draws grid lines, 'click here' text, and live cells before the game starts"
-      (let [state (assoc test-drawing-state :player :stopped)]
-        (with-redefs [q/background (stub :background)
-                      q/fill       (stub :fill)
-                      q/stroke     (stub :stroke)
-                      q/rect       (stub :rect)
-                      q/text-align (stub :text-align)
-                      q/text       (stub :text)]
+  (it "draws background, grid lines, intro text, and live cells before the game starts"
+    (let [state (assoc input-drawing-state :player :stopped)
+          draw  (stub :draw)]
+      (with-redefs [render-shape draw]
+        (->> (all state)
+             (should= expected-shapes-before-game-start)))))
 
-          (all state)
-
-          (assert-background-drawn)
-          (assert-intro-grid-drawn 1)
-          (assert-intro-text-drawn 1)
-          (assert-intro-erased-out 0)
-          (assert-live-cells-drawn))))
-
-
-    (it "only draws live cells after the game has started"
-      (let [state (assoc test-drawing-state :player :playing)]
-        (with-redefs [q/background (stub :background)
-                      q/fill       (stub :fill)
-                      q/stroke     (stub :stroke)
-                      q/rect       (stub :rect)
-                      q/text-align (stub :text-align)
-                      q/text       (stub :text)]
-
-          (all state)
-
-          (assert-background-drawn)
-          (assert-intro-grid-drawn 0)
-          (assert-intro-text-drawn 0)
-          (assert-intro-erased-out 1)
-          (assert-live-cells-drawn))))
-
-    )
+  (it "draws background, grid lines, intro text, and live cells before the game starts"
+    (let [state (assoc input-drawing-state :player :playing)
+          draw  (stub :draw)]
+      (with-redefs [render-shape draw]
+        (->> (all state)
+             (should= expected-shapes-after-game-start)))))
   )
--- src/gui/draw.clj
+++ src/gui/draw.clj
@@ -34,36 +34,83 @@
    :text       0
    :cell       50})
 
-
 (def click-here-text
   "Click a few squares, then click down here to begin!")
 
-(defn intro-grid [state]
-  (when (not (game/playing? state))
-    (let [{:keys [cell-width intro-grid]} (:grid state)]
-      (q/fill (:background color))
-      (q/stroke (:grid-lines color))
-      (doseq [[x y] intro-grid]
-        (q/rect x y cell-width cell-width)))))
+(defn render-rectangle [shape]
+  (let [{:keys [fill stroke x y width height]} shape]
+    (q/fill fill)
+    (q/stroke stroke)
+    (q/rect x y width height)))
 
-(defn intro-text [state]
-  (let [[x y] control-panel-center]
-    (if (game/playing? state)
-      (let [[[x y]] control-panel-bounds]
-        (q/rect x y window-width control-panel-height))
-      (do (q/fill (:text color))
-          (q/text-align :center :center)
-          (q/text click-here-text x y)))))
+(defn render-text [shape]
+  (let [{:keys [fill x-align y-align text x y]} shape]
+    (q/fill fill)
+    (q/text-align x-align y-align)
+    (q/text text x y)))
 
-(defn live-cells [state]
+(defn render-background [shape]
+  (q/background (:color shape)))
+
+(defn render-shape [s]
+  (case (:shape s)
+    :background (render-background s)
+    :rectangle (render-rectangle s)
+    :text (render-text s)))
+
+(defn render-shapes [shapes]
+  (doseq [s shapes]
+    (render-shape s))
+  shapes)
+
+(defn intro-grid-shapes [state]
+  (let [{:keys [cell-width intro-grid]} (:grid state)]
+    (for [[x y] (if (game/playing? state) [] intro-grid)]
+      {:shape  :rectangle
+       :fill   (:background color)
+       :stroke (:grid-lines color)
+       :x      x
+       :y      y
+       :width  cell-width
+       :height cell-width})))
+
+(defn intro-text-shapes [state]
+  (if (game/playing? state)
+    (let [[[x y]] control-panel-bounds]
+      [{:shape  :rectangle
+        :fill   (:cell color)
+        :stroke (:cell color)
+        :x      x
+        :y      y
+        :width  window-width
+        :height control-panel-height}])
+    (let [[x y] control-panel-center]
+      [{:shape   :text
+        :fill    (:text color)
+        :x-align :center
+        :y-align :center
+        :text    click-here-text
+        :x       x
+        :y       y}])))
+
+(defn live-cells-shapes [state]
   (let [{:keys [cell-width live-cells]} (:grid state)]
-    (q/fill (:cell color))
-    (q/stroke (:cell color))
-    (doseq [[[x y] _upper-right] live-cells]
-      (q/rect x y cell-width cell-width))))
+    (for [[[x y] _upper-right] live-cells]
+      {:shape  :rectangle
+       :fill   (:cell color)
+       :stroke (:cell color)
+       :x      x
+       :y      y
+       :width  cell-width
+       :height cell-width})))
+
+(def background-shapes
+  [{:shape :background
+    :color (:background color)}])
 
 (defn all [state]
-  (q/background (:background color))
-  (intro-grid state)
-  (intro-text state)
-  (live-cells state))
+  (render-shapes
+    (concat background-shapes
+            (intro-grid-shapes state)
+            (intro-text-shapes state)
+            (live-cells-shapes state))))
