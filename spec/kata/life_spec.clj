f01ab250545e9b061b201cfecb5983ab194a3903
Michael Whatcott <mdwhatcott@gmail.com>
2021-09-04 21:09:47 -0600

    Incorporated feedback to refine the tests.
    https://michaelwhatcott.com/game-of-life-random-test-inputs/

--- spec/kata/life_spec.clj
+++ spec/kata/life_spec.clj
@@ -3,42 +3,39 @@
             [kata.life :refer :all]))
 
 (def home [1 11])
-(def neighbors (neighbors-of home))
+(defn take-neighbors [n] (->> home neighbors-of shuffle (take n) set))
 
-(describe "The Game of Life Kata"
-  (it "has awareness of potential active neighbor cells"
-    (should= [[0 10] [1 10] [2 10]
-              [0 11] #_home [2 11]
-              [0 12] [1 12] [2 12]] neighbors))
+(describe "Conway's Game of Life"
+  (it "has awareness of the potential active neighbors of a cell"
+    (should= #{[0 10] [0 11] [0 12]
+               [1 10] #_home [1 12]
+               [2 10] [2 11] [2 12]} (take-neighbors 8)))
 
   (for [n (range 9)]
-    (it (format "has awareness of exactly %d active neighbor cells" n)
-      (let [grid (set (take n neighbors))
-            grid (conj grid [42 42])]
+    (it (format "has awareness of %d active neighbors of a cell" n)
+      (let [grid (-> (take-neighbors n) (conj [42 43]))]
         (should= n (count-active-neighbors home grid)))))
 
-  (for [n [0 1 #_"not 2 or 3" 4 5 6 7 8]]
+  (for [n [0 1 #_2 #_3 4 5 6 7 8]]
     (it (format "discards active cells with %d active neighbors" n)
-      (let [grid (set (take n neighbors))
-            grid (conj grid home)]
+      (let [grid (-> (take-neighbors n) (conj home))]
         (should= nil (update-cell home grid)))))
 
   (for [n [2 3]]
     (it (format "retains active cells with %d active neighbors" n)
-      (let [grid (set (take n neighbors))
-            grid (conj grid home)]
+      (let [grid (-> (take-neighbors n) (conj home))]
         (should= home (update-cell home grid)))))
 
-  (it "revives inactive cells with exactly 3 active neighbors"
-    (let [grid (set (take 3 neighbors))]
+  (it "revive inactive cells with exactly 3 active neighbors"
+    (let [grid (take-neighbors 3)]
       (should= home (update-cell home grid))))
 
-  (for [n [0 1 2 #_"not 3" 4 5 6 7 8]]
-    (it (format "ignores inactive cells with %d active neighbors" n)
-      (let [grid (set (take n neighbors))]
+  (for [n [0 1 2 #_3 4 5 6 7 8]]
+    (it (format "ignore inactive cells with %d active neighbors" n)
+      (let [grid (take-neighbors n)]
         (should= nil (update-cell home grid)))))
 
-  (it "evolves an entire grid of cells through multiple iterations"
+  (it "evolves an entire grid of cells"
     (let [oscillator [#{[0 1] [1 1] [2 1]}
                       #{[1 0] [1 1] [1 2]}]]
       (should= (take 8 (cycle oscillator))
